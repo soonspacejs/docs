5909ace5d80aa062cad18ff018217202296c87b3
xuekai-china <ee_shadow@163.com>
2021-04-19 14:18:25 +0800

    add:  docs-iframe component

---
+++ .vuepress/components/Docs/Iframe.vue
@@ -0,0 +1,29 @@
+<template>
+  <iframe
+    class="docs-iframe"
+    :src="`${this.baseUrl}${this.src}`"
+    frameborder="0"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: "docs-iframe",
+  props: {
+      src: String
+  },
+  data() {
+    return {
+      baseUrl: "http://www.xwbuilders.com:9018/soonspacejs/examples/page/2.x/"
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.docs-iframe 
+  width: 100%
+  height: 400px
+  padding: 12px 0
+
+</style>
