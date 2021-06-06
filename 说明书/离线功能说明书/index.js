260a8709d54452e8d4ce7b88bbea01ae9edee775
xuekai-china <ee_shadow@163.com>
2021-06-06 18:48:22 +0800

    add: 离线功能说明书

---
+++ 说明书/离线功能说明书/index.js
@@ -0,0 +1,75 @@
+const PDFMerger = require('pdf-merger-js');
+const fs = require('fs')
+const path = require('path')
+
+const filesDir = path.resolve(__dirname, './file')
+const files = fs.readdirSync(filesDir)
+
+const merger = new PDFMerger();
+const name = 'SoonSpace 功能说明书.pdf';
+
+function afterMinBefore(before, after) {
+  let res = false
+
+  const length = after.length > before.length ? after.length : before.length
+
+  for (let i = 0; i < length; i++) {
+
+    if (!before[i]) {
+      res = false
+      break
+    } else if (!after[i]) {
+      res = true
+      break
+    } else if (after[i] < before[i]) {
+      res = true
+      break
+    } else if (after[i] > before[i]) {
+      res = false
+      break
+    }
+  }
+
+  return res
+}
+
+function resort(files) {
+
+  const sortDataArr = []
+
+  for (let i in files) {
+    const iIndex = files[i].split(' ')[0]
+
+    sortDataArr.push({
+      key: iIndex,
+      indexs: iIndex.split('.').map(i => Number(i)),
+      dataIndex: i
+    })
+  }
+
+  console.log(sortDataArr)
+
+  for (let i = 0; i < sortDataArr.length - 1; i++) {
+    for (let j = 0; j < sortDataArr.length - i - 1; j++) {
+      if (afterMinBefore(sortDataArr[j].indexs, sortDataArr[j+1].indexs)) {
+        const temp = sortDataArr[j]
+        sortDataArr[j] = sortDataArr[j+ 1]
+        sortDataArr[j+ 1] = temp
+      }
+    }
+
+  }
+
+  return sortDataArr.map(i => files[i.dataIndex])
+}
+
+(async () => {
+  const newFiles = resort(files)
+
+  console.log(files, newFiles)
+
+  for (let i in newFiles) {
+    merger.add(`file/${newFiles[i]}`);
+  }
+  await merger.save(`dist/${name}`);
+})();
