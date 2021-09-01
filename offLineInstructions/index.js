144d13b4b58fd59085325f014a5a4d7051fbe0a8
xuekai-china <ee_shadow@163.com>
2021-09-01 11:02:25 +0800

    chore: perf offLineInstructions code

--- offLineInstructions/index.js
+++ offLineInstructions/index.js
@@ -10,14 +10,20 @@ if (DS_StoreIndex > -1) files.splice(DS_StoreIndex, 1)
 
 const merger = new PDFMerger();
 const destName = 'SoonSpace 功能说明书.pdf';
+const outputPath = `./.vuepress/public/resource/离线功能说明书/${destName}`
 
-function afterMinBefore(before, after) {
+/**
+ * 比较前后俩个纯数字数组是否需要反转
+ * @param  {Array<Number>} before
+ * @param  {Array<Number>} after
+ * @return {boolean}
+ */
+function isNeedReverse(before, after) {
   let res = false
 
   const length = after.length > before.length ? after.length : before.length
 
   for (let i = 0; i < length; i++) {
-
     if (!before[i]) {
       res = false
       break
@@ -36,23 +42,34 @@ function afterMinBefore(before, after) {
   return res
 }
 
-function resort(files) {
+function sortFile(files) {
 
-  const sortDataArr = []
-
-  for (let i in files) {
-    const iIndex = files[i].split(' ')[0]
-
-    sortDataArr.push({
-      key: iIndex,
-      indexs: iIndex.split('.').map(i => Number(i)),
-      dataIndex: i
-    })
-  }
+  // sortDataArr = [
+  //   {
+  //     indexs: [0],
+  //     dataIndex: 0
+  //   },
+  //   {
+  //     indexs: [2, 10],
+  //     dataIndex: 1
+  //   },
+  //   {
+  //     indexs: [2, 1],
+  //     dataIndex: 2
+  //   },
+  //   {
+  //     indexs: [2, 7, 2],
+  //     dataIndex: 3
+  //   },
+  // ]
+  const sortDataArr = files.map((item, dataIndex) => ({
+    indexs: item.split(' ')[0].split('.').map(i => Number(i)),
+    dataIndex
+  }))
 
   for (let i = 0; i < sortDataArr.length - 1; i++) {
     for (let j = 0; j < sortDataArr.length - i - 1; j++) {
-      if (afterMinBefore(sortDataArr[j].indexs, sortDataArr[j + 1].indexs)) {
+      if (isNeedReverse(sortDataArr[j].indexs, sortDataArr[j + 1].indexs)) {
         const temp = sortDataArr[j]
         sortDataArr[j] = sortDataArr[j + 1]
         sortDataArr[j + 1] = temp
@@ -61,14 +78,14 @@ function resort(files) {
 
   }
 
-  return sortDataArr.map(i => files[i.dataIndex])
+  return sortDataArr.map(item => files[item.dataIndex])
 }
 
 (async () => {
-  const newFiles = resort(files)
+  const newFiles = sortFile(files)
 
   for (let i in newFiles) {
     merger.add(path.resolve(__dirname, `./file/${newFiles[i]}`));
   }
-  await merger.save(`./.vuepress/public/resource/离线功能说明书/${destName}`);
+  await merger.save(outputPath);
 })();
